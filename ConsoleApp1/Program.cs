7182d596bac22b67d405519e084936ca897931a3
LaVaughnGit <113532445+LaVaughnGit@users.noreply.github.com>
2026-07-03 17:15:10 -0400

    first commit on c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+Console.WriteLine($"{Environment.NewLine}Hello, {name}! Today is {currentDate:d}.");
+Console.WriteLine($"The current time is {currentDate:T}.");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey(true);
