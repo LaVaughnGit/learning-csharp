1579dc689df44b30525fbac1c710c2098d4a12a0
LaVaughnGit <113532445+LaVaughnGit@users.noreply.github.com>
2026-07-04 10:58:38 -0400

    data type learning

--- ConsoleApp1/firstProgram.cs
+++ ConsoleApp1/firstProgram.cs
@@ -22,6 +22,36 @@ namespace HelloWorld
              * Multi-line comments noice.
              * 
              */
+
+            int y1 = 10;
+            Console.WriteLine($"The value of y1 is {y1}.");
+            double y2 = 20.1;
+            Console.WriteLine($"The value of y2 is {y2}.");
+            int y3 = 30;
+            Console.WriteLine($"The value of y3 is {y3}.");
+            bool y4 = true;
+            Console.WriteLine($"The value of y4 is {y4}.");
+
+            char a = 'A';
+            Console.WriteLine($"The value of a is {a}.");
+            string b = "Whats up!";
+            Console.WriteLine($"The value of b is {b}.");
+
+            const double pi = 3.14159;
+            Console.WriteLine($"pi is {pi}.");
+
+            //pi = 3.14; // This line will cause a compile-time error because pi is a constant and cannot be reassigned.
+
+            int x1, x2, x3;
+            x1 = 5;
+            x2 = 10;
+            x3 = x1 + x2;
+            Console.WriteLine($"The value of x3 is {x3}.");
+
+            // Explicit Conversion
+            double d = 9.78;
+            int i = (int)d; // Changes double to int, truncating the decimal part
+            Console.WriteLine($"The value of i is {i}.");
         }
     }
 }
